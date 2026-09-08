c9289d742bdc1f9f3ded2187324077f131d4ac58
Connpr1 <connor.rzasa@icloud.com>
2026-09-08 18:22:25 +0000

    my first commit

---
+++ crash/crash-course.py
@@ -0,0 +1,35 @@
+# comment
+
+# ctr
+# foward
+# slash
+
+print ("Hello World!")
+
+# Variable Declerations and Data Types:
+
+a = 4          # Intiger
+b = 5.5        # Floats
+c = "CSAEA"    # Strings
+D = False      # Boolean
+
+print(a, b, c, D)
+
+
+#Operators
+# + - / * % ** //
+# += -= /= 
+
+e = 11 % 10
+print(e)
+e += 7
+print(e)
+
+# f-string
+
+print(f"e is equal to {e}")
+
+e -= 7
+e += 12
+
+print(f"e is NOW equal to {e}")
