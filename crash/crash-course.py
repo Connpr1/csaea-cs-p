f280257a997ae34f3e8870b7386bdbc38e4d3cee
Connpr1 <connor.rzasa@icloud.com>
2026-09-10 16:26:42 +0000

    Crashcourse day 2

--- crash/crash-course.py
+++ crash/crash-course.py
@@ -1,3 +1,5 @@
+import math
+
 # comment
 
 # ctr
@@ -33,3 +35,52 @@ e -= 7
 e += 12
 
 print(f"e is NOW equal to {e}")
+
+# COMPARISONS (booleans, which always return True or False)
+
+# < >  <= >=  == !=
+
+print(4 < 5)
+print(7 == 4)
+print(1 != 2)
+
+isEqual = "Yes" != "yes"
+print(isEqual)
+
+
+# Logical Operators
+# In order of precedence: not and or
+
+f = False
+t = True
+
+#Predict output
+print(not f)             # True
+print(f and t)           # False
+print(f or t)            # True
+print(f or t and not f)  # True
+
+#Casting
+
+g = int(5.5) # Will chop off everything after the decimal
+h = str(4)
+print(g, h)
+
+# Strings
+
+s1 = "Goodnight"
+s2 = " and "
+s3 = "Goodbye"
+end = s1 + s2 + s3  # concatination with +
+end += ", Cowboy."
+
+
+print(end + "\n") 
+
+# MATH LIBRARY??
+# All the way at top
+
+print(math.sqrt(14))  #Square Root
+print(math.ceil(3.65)) #Round Up
+print(math.floor(7.43)) # Round Down
+print(math.pow(2, 4))   #Squares It
