c728978b5a40840a3ce5a3fd760ccd16f21e6c9e
Connpr1 <connor.rzasa@icloud.com>
2026-09-11 17:20:51 +0000

    Commit 3

--- crash/crash-course.py
+++ crash/crash-course.py
@@ -74,7 +74,6 @@ s3 = "Goodbye"
 end = s1 + s2 + s3  # concatination with +
 end += ", Cowboy."
 
-
 print(end + "\n") 
 
 # MATH LIBRARY??
@@ -83,4 +82,59 @@ print(end + "\n")
 print(math.sqrt(14))  #Square Root
 print(math.ceil(3.65)) #Round Up
 print(math.floor(7.43)) # Round Down
-print(math.pow(2, 4))   #Squares It
+print(math.pow(2, 4))   # X^Y
+
+# Conditionals
+# if elif else
+
+t = True
+f = False
+
+if f: 
+    print("Reached the first condition")
+elif t:
+    print("Reached second condition")
+else: 
+    print("Reached else")
+
+if 1 > 1 and 1 == 1:
+     print("Reached the first condition")
+elif 6 == 7 or 3!=3:
+       print("Reached second condition")
+elif 10!=10:
+      print("Reached third condition")       
+else:
+     print("Reached else")
+
+#LISTS
+# a list can hold any type, and can grow or shrink anytime
+
+#Index: 0   1   2  3   4
+nums = [34, 52, 3, 64, 32]
+
+print(nums)
+print(nums[3]) #Predict
+print(nums[0])
+print(nums[-1])
+print(nums[-3])
+print(nums[0] + nums[2])
+
+nums[0] = 64
+print(nums)
+
+#Lists Methods
+#Special built in methods
+
+words = []
+
+words.append("Word 1")
+words.append("Word 2")
+words.append("Word 3")
+print(words)
+
+words.remove("Word 1")
+words.insert(0, "Word 4")
+words[1] = "Word 5"
+length = len(words)
+print(words)
+print(length)
