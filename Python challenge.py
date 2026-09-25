6f00b32a1fd0e9d58311af8f5343ac9cbeae3ab3
Connpr1 <connor.rzasa@icloud.com>
2026-09-25 18:23:56 +0000

    hh

--- Python challenge.py
+++ Python challenge.py
@@ -3,3 +3,21 @@ import math
 fahrenheit = 212
 Celcious = (fahrenheit - 32) * 5/9
 print("Hello, todays weather is gonna be a hot one at",Celcious)
+
+
+Attempt =  123456
+Password = 12345
+
+if Attempt := Password:
+    print("Access Granted")
+
+elif Password != Attempt:
+    print("Access Denied")
+
+First = "Tommy"
+Last = "R"
+From = "Rust"
+
+username = First + Last + From
+
+print(username, "Joined The Game")
